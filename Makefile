44449e560da0c74b420c3167ddde80bbf70d5bdc
Ricardo Branco <rbranco@suse.de>
2020-10-24 11:06:50 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+generate:
+	@python3 pki
+
+verify:
+	@openssl verify -x509_strict -CAfile cacerts.pem client.pem server.pem cacerts.pem
+
+clean:
+	@rm -f *.pem *.key
+
+test:
+	@pylint pki *.py
+	@flake8 pki *.py
+	@make generate
+	@make verify
+	@make clean
---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Setup script
+"""
+
+from setuptools import setup
+
+
+def read(path):
+    """
+    Read a file
+    """
+    with open(path) as file:
+        return file.read()
+
+
+setup(
+    name='simplepki',
+    version="0.1.0a",
+    description="Simple PKI for testing TLS",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    author="Ricardo Branco",
+    author_email='rbranco@suse.de',
+    url='https://github.com/ricardobranco777/simplepki',
+    package_dir={'simplepki': '.'},
+    # packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.5',
+    install_requires=read('requirements.txt'),
+    license='MIT License',
+    zip_safe=False,
+    keywords='simplepki',
+    scripts=['simplepki'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+        'License :: OSI Approved :: '
+        'MIT License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
